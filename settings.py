9eaeee63cf2263ca6f691ad8b8ce9af658314a6b
dkimuene <68829358+dkimuene@users.noreply.github.com>
2020-07-28 15:42:16 -0500

    Few color changes in setting class

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings:
         #Screen setting 
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230,0,230)
+        self.bg_color = (230,230,230)
