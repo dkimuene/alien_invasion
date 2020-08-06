bd04c81ddf86cec4452208614c9542637a0f1f36
dkimuene <68829358+dkimuene@users.noreply.github.com>
2020-08-06 15:10:59 -0500

    The game now has a highscore display and tracker

--- alien_invasion.py
+++ alien_invasion.py
@@ -132,6 +132,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destory existing bullet and create new fleet
--- game_stats.py
+++ game_stats.py
@@ -10,6 +10,8 @@ class GameStats:
         #Start Alien Invasion in an active state.
         self.game_active = False
 
+        # High score should never be reset.
+        self.high_score = 0
 
     def reset_stat(self):
         """Initialize statistics that can change during the game."""
