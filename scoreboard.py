077cd44f5d8bdf5a8fc1db2e4d87e8891c3c9c74
dkimuene <68829358+dkimuene@users.noreply.github.com>
2020-08-06 15:48:04 -0500

    The game now has a level display and tracker.

--- scoreboard.py
+++ scoreboard.py
@@ -16,6 +16,7 @@ class Scoreboard:
         # Prepare the initial score image.
         self.prep_score()
         self.prep_high_score()
+        self.prep_level()
 
     def prep_score(self):
         """Turn the score into a rendered image."""
@@ -27,6 +28,19 @@ class Scoreboard:
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
+
+    def prep_level(self):
+        """Turn the level into a rendered image."""
+        level_str = str(self.stats.level)
+        self.level_image = self.font.render(level_str, True, 
+                self.text_color, self.settings.bg_color)
+        
+        # Position the level below the score.
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
+        self
 
     def prep_high_score(self):
         """ Turn the highscore into a rendered image."""
@@ -41,9 +55,10 @@ class Scoreboard:
         self.high_score_rect.top = self.score_rect.top 
 
     def show_score(self):
-        """Draw score to the screen."""
+        """Draw score and level to the screen."""
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     def check_high_score(self):
         """ Check to see if there's a new high score."""
